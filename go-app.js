87570c8041eec54b4aa00c08e25297ee1393ae12
Munsanje Mweene <mmweene15@gmail.com>
2016-07-01 15:52:57 +0200

    Removed token from config.json

--- go-app.js
+++ go-app.js
@@ -5,39 +5,43 @@
 var go = {};
 go;
 
-var vumigo = require('vumigo_v02');
-var JsonApi = vumigo.http.api.JsonApi;
+go.utils = function() {
+  var vumigo = require('vumigo_v02');
+  var JsonApi = vumigo.http.api.JsonApi;
 
-go.utils = {
-    converse: function(im, SESSION_ID, content) {
-                  var http = new JsonApi(im, {
-                      headers: {
-                        'Authorization': ['Bearer ' + im.config.wit.token],
-                        'Accept': ['application/vnd.wit.' + im.config.wit.version + "+json"],
-                        'Content-Type': ['application/json']
-                      }
-                  });
-                  return http.post('https://api.wit.ai/converse?', content === null ?
-                                    {
-                                        params: {
-                                          v: im.config.wit.version, // write method that extracts version
-                                          session_id: SESSION_ID
-                                        }
-                                    } :
-                                    {
-                                      params: {
-                                        v: im.config.wit.version, // write method that extracts version
-                                        session_id: SESSION_ID,
-                                        q: content
-                                      }
-                                  }
-                              )
-                              .then(function(response) {
-                                  return response;
-                              });
+  var converse = function(im, SESSION_ID, content) {
+      var http = new JsonApi(im, {
+        headers: {
+          'Authorization': ['Bearer ' + im.config.wit.token],
+          'Accept': ['application/vnd.wit.' + im.config.wit.version + "+json"],
+          'Content-Type': ['application/json']
+        }
+      });
+      return http.post('https://api.wit.ai/converse?', content === null ?
+      {
+        params: {
+          v: im.config.wit.version, // write method that extracts version
+          session_id: SESSION_ID
+        }
+      } :
+      {
+        params: {
+          v: im.config.wit.version, // write method that extracts version
+          session_id: SESSION_ID,
+          q: content
+        }
+      }
+    )
+    .then(function(response) {
+      return response;
+    });
 
-    }
-};
+  };
+
+  return {
+    converse: converse
+  };
+}();
 
 go.app = function() {
     var vumigo = require('vumigo_v02');
